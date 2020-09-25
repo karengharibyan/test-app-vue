40d85d0fb4767dd73fb7b46f75a94d3996d8a93d
Karen Gharibyan <karen.gharibyan3@icluod.com>
2020-09-25 18:53:25 +0400

    added signIn Modal

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Header />
     <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
       <transition
         name="fade"
         mode="out-in"
       >
       <router-view />
       </transition>
+      </v-container>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-
+import Header from '@/components/app/Header.vue'
+import Footer from '@/components/app/Footer.vue'
 export default {
   name: 'App',
 
   data: () => ({
     //
   }),
+  components: {
+    Header,
+    Footer
+  },
   mounted(){
     console.log(this.$auth.login({
       data: {email: 'birdie.koepp@example.com',password: "password"}
---
+++ src/components/auth/SignInModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="primary" dark v-bind="attrs" v-on="on"> Sign In </v-btn>
+    </template>
+    <v-card>
+      <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                  v-model="email"
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    :rules="emailRules"
+                  ></v-text-field>
+
+                  <v-text-field
+                  v-model="password"
+                    :rules="passwordRules"
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+
+<script>
+export default {
+  name: "SignInModal",
+  data: () => ({
+    dialog: false,
+      valid: false,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+      ],
+  }),
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
